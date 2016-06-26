53b87f9752ae5f5ab1534e1f7a713d97fe1a8c34
Expost <tpxkldh@163.com>
2016-06-27 01:05:42 +0800

    finished

--- add_word.py
+++ add_word.py
@@ -8,7 +8,7 @@ import sys
 
 
 class AddShanbayWord:
-	def __init__(self,username,password,book_url,unit_name_list = [],unit_url,file_name):
+	def __init__(self,username,password,book_url,unit_name_list,unit_url,file_name):
 		self.username = username
 		self.password = password
 		self.book_url = book_url
@@ -66,11 +66,15 @@ class AddShanbayWord:
 			print word,u"添加失败由于: " + j["msg"]
 			defor_array = self.get_deformation(word)
 			for i in defor_array:
+				#self.add_word(cookie,uid,i)
 				payload["word"] = i
 				r = requests.post(add_url,data = payload,cookies = cookie)
 				j = json.loads(r.content)
 				if j["status_code"] == 0:
 					print "   ",i,u"尝试添加成功"
+				elif u"上限" in j["msg"]:
+					print "   ",i,u"尝试添加失败由于: " + j["msg"]
+					return True
 				else:
 					print "   ",i,u"尝试添加失败由于: " + j["msg"]
 				time.sleep(0.1)
@@ -83,9 +87,14 @@ class AddShanbayWord:
 		payload["description"] = "+"
 		payload["wordbook_id"] = book_id
 		r = requests.post(unit_url,data = payload,cookies = cookie)
-		print r.status_code
+		if r.status_code != 200:
+			print u"添加单词单元%s错误" %unit_name
+			print r.status_code
+			exit(-1)
 		j = json.loads(r.content)
 		if j["status_code"] == 0:
+			print u"添加单元%s成功" % unit_name
+			print u"\n------------------%s---------------\n" % unit_name
 			return j["data"]["wordlist"]["id"]
 		else:
 			return None
@@ -96,19 +105,18 @@ class AddShanbayWord:
 		word_text = open(self.file_name).read()
 		r = re.compile("[a-zA-Z]+")
 		word_list = r.findall(word_text)
+		book_id = self.book_url.split("/")[-2]
 		if self.unit_url:
 			uid = self.unit_url.split("/")[-2]
 			self.add_word_to_oneunit(cookie,uid,word_list,0)
 		elif self.unit_name_list:
 			index = 0
 			for lname in self.unit_name_list:
-				
 				uid = self.add_word_unit(cookie,book_id,lname)
 				index = self.add_word_to_oneunit(cookie,uid,word_list,index)
 		else:
 			unit_num = 1
 			index = 0
-			book_id = self.book_url.split("/")[-1]
 			while 1:
 				uid = self.add_word_unit(cookie,book_id,str(unit_num))
 				index = self.add_word_to_oneunit(cookie,uid,word_list,index)
@@ -125,24 +133,25 @@ class AddShanbayWord:
 
 def main():
 	try:
-		j = json.load(open(file_name))
+		j = json.load(open("config.json"))
 	except Exception,e:
-		print "load config error",e
+		print "load config error:",e
 		exit(-1)
 	username = j["username"]
 	password = j["password"]
 	book_url = j["bookurl"]
-	auto_addunit = j["autoaddunit"]
 	unit_name = j["unitname"]
 
-	if len(sys.argv) > 2:
+	if len(sys.argv) == 3:
 		a = AddShanbayWord(username,password,book_url,[],sys.argv[1],sys.argv[2])
 		a.start_add()
-	elif auto_addunit and len(sys.argv) > 1:
+	elif len(sys.argv) == 2:
 		a = AddShanbayWord(username,password,book_url,unit_name,"",sys.argv[1])
 		a.start_add()
 	else:
-		print "需要指定配置文件autoaddunit参数，否则使用命令行指定单词书某一单元的链接地址"
+		print u"命令行参数不正确。"
+		print u"请按如下方法使用"
+		print u"add_word.py [[unit_url]] [word.txt]"
 
 if __name__ == "__main__":
 	main()
